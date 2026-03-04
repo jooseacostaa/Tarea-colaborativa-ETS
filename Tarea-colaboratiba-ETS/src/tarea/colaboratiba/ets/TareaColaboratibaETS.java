8f4857cbcc363f561c049c8f9a0e45efcf1640f8
jooseacostaa <joseacostanaranjo@hotmail.com>
2026-03-04 16:33:31 +0000

    Updating main

--- Tarea-colaboratiba-ETS/src/tarea/colaboratiba/ets/TareaColaboratibaETS.java
+++ Tarea-colaboratiba-ETS/src/tarea/colaboratiba/ets/TareaColaboratibaETS.java
@@ -12,6 +12,42 @@ public class TareaColaboratibaETS {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        GestorTareas gestor = new GestorTareas();
+        int opcion;
+
+        do {
+            System.out.println("\n--- GESTOR DE TAREAS ---");
+            System.out.println("1. Añadir tarea");
+            System.out.println("2. Listar tareas");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Introduce la descripción de la tarea: ");
+                    String descripcion = sc.nextLine();
+                    gestor.añadirTarea(descripcion);
+                    break;
+
+                case 2:
+                    gestor.mostrarTareas();
+                    break;
+
+                case 0:
+                    System.out.println("Saliendo del programa...");
+                    break;
+
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+        } while (opcion != 0);
+
+        sc.close();
+
     }
 
 }
