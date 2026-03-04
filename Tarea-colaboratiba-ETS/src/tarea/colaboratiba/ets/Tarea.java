6fe0f1a633f7a2e2beae00ba9a9fc1efcaa75d29
jooseacostaa <joseacostanaranjo@hotmail.com>
2026-03-04 16:08:50 +0000

    Adding Javadoc Info

--- Tarea-colaboratiba-ETS/src/tarea/colaboratiba/ets/Tarea.java
+++ Tarea-colaboratiba-ETS/src/tarea/colaboratiba/ets/Tarea.java
@@ -2,9 +2,21 @@ package tarea.colaboratiba.ets;
 
 public class Tarea {
 
+    /**
+     * Clase que representa una tarea dentro del sistema. Contiene una
+     * descripción y un estado de completado.
+     *
+     * @author Jose
+     */
     public String descripcion;
     public boolean completada;
 
+    /**
+     * Constructor de la clase Tarea.
+     *
+     * @param descripcion Texto descriptivo de la tarea.
+     * @param completada Variable boolean iniciada en falso.
+     */
     public Tarea(String descripcion, boolean completada) {
         this.descripcion = descripcion;
         this.completada = false;
